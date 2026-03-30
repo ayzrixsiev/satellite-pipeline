7ba337e68075f4285f85c62730701c9e43d09c89
Aziz Rixsiev <ayzrixsiev@gmail.com>
2026-03-30 13:19:13 +0500

    chore: Set up ingest.py for pipeline

---
+++ pipeline/ingest.py
@@ -0,0 +1,36 @@
+"""Find a necessary data, check the data type (tiff) and then find image and it's label pair"""
+
+import os
+
+
+class DataIngestor:
+    def __init__(self, raw_images, raw_labels):
+        self.raw_images = raw_images
+        self.raw_labels = raw_labels
+        self.data_pair = []
+
+    def make_pairs(self):
+        image_files = os.listdir(self.raw_images)
+        label_files = os.listdir(self.raw_labels)
+        # Creating a dict of {"Stem": "Full file name"}, stem is a name before .tif
+        label_map = {os.path.splitext(f)[0]: f for f in label_files}
+        for image in image_files:
+            image_stem = os.path.splitext(image)[
+                0
+            ]  # Get the name of an image without it's extension .tiff
+            if image_stem in label_map:
+                matching_label = label_map[image_stem]
+                full_image_path = os.path.join(self.raw_images, image)
+                full_label_path = os.path.join(self.raw_labels, matching_label)
+                self.data_pair.append((full_image_path, full_label_path))
+
+        return self.data_pair
+
+    def get_data(self):
+        return self.data_pair
+
+
+data_ingestor = DataIngestor("../data/tiff/train", "../data/tiff/train_labels")
+data_ingestor.make_pairs()
+paired_data = data_ingestor.get_data()
+print(paired_data)
